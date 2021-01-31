c07558aa6d9276c638d2c477a8f22eb9c0c97c83
officeyutong <yt.xyxx@gmail.com>
2021-01-31 22:21:11 +0800

    modal update

--- src/app/dialogs/Dialog.tsx
+++ src/app/dialogs/Dialog.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Modal, Header, Message, Button, Confirm } from "semantic-ui-react";
+import { Modal, Header, Message, Button, Confirm, Transition } from "semantic-ui-react";
 type StateType = {
     showing: boolean;
     message: string;
     title: string;
     error: boolean;
     success: boolean;
+    showTransition: boolean;
 
 };
 type PropType = {
@@ -23,39 +24,51 @@ class MyMessageBox extends React.Component<PropType, StateType> {
         message: "",
         title: "标题",
         error: true,
-        success: false
+        success: false,
+        showTransition: false
     }
     public constructor(props: PropType) {
         super(props);
         this.state = {
-            showing: true,
+            showing: false,
+            showTransition: false,
             ...props
 
         };
     }
+    componentDidMount() {
+        this.setState({ ...this.state, showing: true })
+    }
     render() {
-        return (<Modal
-            open={this.state.showing}
-            size="tiny"
-            closeOnDimmerClick={true}
-            closeOnDocumentClick={true}
+        return (<Transition
+            visible={this.state.showing}
+            animation="fade"
+            duration={300}
+            unmountOnHide={true}
         >
-            <Header content={this.state.title} />
-            <Modal.Content>
-                <Message error={this.state.error} success={!this.state.error}>
-                    <Header>
-                        <h3>{this.state.error ? "错误" : "完成"}</h3>
-                    </Header>
-                    <div dangerouslySetInnerHTML={{ __html: this.state.message }}>
+            <Modal
+                open={true}
+                size="tiny"
+                closeOnDimmerClick={false}
+                closeOnDocumentClick={true}
+            >
+                <Header content={this.state.title} />
+                <Modal.Content>
+                    <Message error={this.state.error} success={!this.state.error}>
+                        <Header>
+                            <h3>{this.state.error ? "错误" : "完成"}</h3>
+                        </Header>
+                        <div dangerouslySetInnerHTML={{ __html: this.state.message }}>
 
-                    </div>
-                </Message>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button as="a" color="blue" href="/">返回主页</Button>
-                <Button color="green" onClick={() => this.setState({ showing: false })}>关闭</Button>
-            </Modal.Actions>
-        </Modal>);
+                        </div>
+                    </Message>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button as="a" color="blue" href="/">返回主页</Button>
+                    <Button color="green" onClick={() => this.setState({ showing: false })}>关闭</Button>
+                </Modal.Actions>
+            </Modal>
+        </Transition>);
     }
 };
 
